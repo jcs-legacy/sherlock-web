1adb94f687caf1b61b0b6e6b9106f44726ebab06
JenChieh <jcs090218@gmail.com>
2021-01-06 20:13:15 +0800

    Working version 1.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 "use strict";
 
 var textarea = document.getElementById('text-area');
+var btn_search = document.getElementById('btn-search');
+var tb_search = document.getElementById('tb-search');
+var sherlock_data = { };
 
 function append(str) {
   textarea.innerHTML += str;
@@ -9,18 +12,36 @@ function append(str) {
   textarea.rows += count;
 }
 
-function erase() { textarea.innerHTML = ''; }
+function erase() { textarea.innerHTML = ''; textarea.rows = 0; }
 
-(function ($) {
-  $.ajax({
-    url: '127.0.0.1:8000/data',
-    type: 'GET',
-    contentType: "application/json",
-    success : function (data) {
-      
-    },
-    error : function () {
+function request(type, url, callback, body) {
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState != 4 || this.status != 200)
+      return;
+    callback(this);
+  };
+  xhttp.open(type, url, true);
+  xhttp.send(body);
+}
 
-    },
+function init() {
+  request("GET", "http://localhost:8000/data/", function (xhr) {
+    sherlock_data = JSON.parse(xhr.response);
   });
-}(this.jQuery));
+}
+init();
+
+function search() {
+  erase();
+  let args = tb_search.value;
+  request("POST", "http://localhost:8000/cli/", function (xhr) {
+    let result = JSON.parse(xhr.response);
+    append(result['output']);
+  }, JSON.stringify({ args: args }));
+}
+
+tb_search.onkeyup = function(event) {
+  if (event.keyCode === 13) { search(); }
+};
+btn_search.onclick = search;
